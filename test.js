46ae5eba107fc0478a72ba2338c17b6a12969638
Ali <alykhan027@gmail.com>
2025-09-16 03:00:14 -0400

    Help to see working of pipeline

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 console.log("Strtarting test...");
-await setTimeout(() => {
-    console.log("Running tests...");
+setTimeout(() => {
+    console.log("Tests completed after 3s.");
 }, 3000);
-console.log("Test completed.");
+console.log("Running tests...");
